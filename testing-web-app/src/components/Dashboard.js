9d44196bf55f985d38459b44106f95b63b1dfd70
Elijah Paminsan <e.paminsan@gmail.com>
2019-09-13 14:33:36 -0700

    refactored call backs to export them for testing

--- testing-web-app/src/components/Dashboard.js
+++ testing-web-app/src/components/Dashboard.js
@@ -1,47 +1,54 @@
 import React, { useState } from 'react'
 import Display from './Display'
 
-function Dashboard() {
+// callbacks
+export const addBall = currentState => currentState + 1
+export const addStrike = currentState => currentState + 1
+export const addHit = currentState => currentState + 1
+export const addFoul = currentState => currentState + 1
+
+const Dashboard = () => {
+  // setting states for ball, strike, hit, and foul
   const [ball, setBall] = useState(0)
   const [strike, setStrike] = useState(0)
   const [hit, setHit] = useState(0)
   const [foul, setFoul] = useState(0)
 
-  const addBall = () => {
-    return ball < 4 ? setBall(ball + 1) : setBall(0)
-  }
+  // if ball is less than four, set ball to ball + 1 -- if not, reset ball count
+  const callBall = () => (ball < 4 ? setBall(addBall(ball)) : setBall(0))
 
-  const addStrike = () => {
-    return strike < 3 ? setStrike(strike + 1) : setStrike(0)
-  }
+  // if strikes are less than 3, set strike to strike + 1 -- if not, reset strike count
+  const callStrike = () =>
+    strike < 3 ? setStrike(addStrike(strike)) : setStrike(0)
 
-  const addHit = () => {
-    return setStrike(0) & setBall(0) & setHit(hit + 1)
-  }
+  // everytime the ball is hit, reset strike and ball counts. +1 to hit count
+  const callHit = () => setStrike(0) & setBall(0) & setHit(addHit(hit))
 
-  const addFoul = () => {
+  const callFoul = () => {
     if (strike === 0) {
-      return setFoul(foul + 1) & addStrike(strike + 1)
+      // if strike count 0, add 1 strike and 1 fould
+      return setFoul(addFoul(foul)) & setStrike(addStrike(strike))
     } else if (strike === 1) {
-      return setFoul(foul + 1) & addStrike(strike + 2)
+      // if strike count is 1, add 1 foul and 1 to strike
+      return setFoul(addFoul(foul)) & setStrike(addStrike(strike))
     } else if (strike === 2) {
-      return setFoul(foul + 1)
+      // if strike count is 2, add 1 foul
+      return setFoul(addFoul(foul))
     }
   }
 
-  const reset = () => {
-    return setBall(0) & setStrike(0) & setHit(0) & setFoul(0)
-  }
+  // reset all counts to 0
+  const reset = () => setBall(0) & setStrike(0) & setHit(0) & setFoul(0)
 
   return (
     <>
       <Display balls={ball} strikes={strike} hit={hit} foul={foul} />
       <div className='controls'>
         <div className='btn-holder'>
-          <button onClick={addBall}>ball</button>
-          <button onClick={addStrike}>strike</button>
-          <button onClick={addHit}>hit</button>
-          <button onClick={addFoul}>foul</button>
+          <button onClick={callBall}>ball</button>
+          <button onClick={callStrike}>strike</button>
+          <button onClick={callHit}>hit</button>
+          <button onClick={callFoul}>foul</button>
           <button onClick={reset}>reset</button>
         </div>
       </div>
@@ -50,3 +57,13 @@ function Dashboard() {
 }
 
 export default Dashboard
+
+// const addFoul = () => {
+//   if (strike === 0) {
+//     return setFoul(foul + 1) & addStrike(strike + 1)
+//   } else if (strike === 1) {
+//     return setFoul(foul + 1) & addStrike(strike + 2)
+//   } else if (strike === 2) {
+//     return setFoul(foul + 1)
+//   }
+// }
